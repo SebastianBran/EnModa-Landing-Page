0586f95c6d59a6847fc1fffcb36b691eeeb3eaa0
franklin-sanchez <57018765+franklin-sanchez@users.noreply.github.com>
2021-06-13 21:22:19 -0500

    Update scripts.js

--- js/scripts.js
+++ js/scripts.js
@@ -1,9 +1,52 @@
-$(document).ready(function() {
-    $('#autoWidth').lightSlider({
-        autoWidth:true,
-        loop:true,
-        onSliderLoad: function() {
-            $('#autoWidth').removeClass('cS-hidden');
-        }
+const mainMenu = document.querySelector('.mainMenu');
+const closeMenu = document.querySelector('.closeMenu');
+const openMenu = document.querySelector('.openMenu');
+
+
+
+
+openMenu.addEventListener('click',show);
+closeMenu.addEventListener('click',close);
+
+function show(){
+    mainMenu.style.display = 'flex';
+    mainMenu.style.top = '0';
+}
+function close(){
+    mainMenu.style.top = '-100%';
+}
+
+/*PRODUCTOS*/
+window.addEventListener('load', function (){
+    new Glider(document.querySelector('.carousel__lista'),{
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        draggable: true,
+        dots: '.carousel__indicadores',
+        arrows: {
+            prev: '.carousel__anterior',
+            next: '.carousel__siguiente'
+        }, responsive:
+            [
+                {
+                    // screens greater than >= 1024px
+                    breakpoint: 600,
+                    settings: {
+                        slidesToShow: 2,
+                        slidesToScroll: 2,
+                    }
+                },
+
+                {
+                    // screens greater than >= 600px
+                    breakpoint: 1160,
+                    settings: {
+                        slidesToShow: 4,
+                        slidesToScroll: 4,
+                    }
+                }
+
+            ]
     });
-});
+
+})
